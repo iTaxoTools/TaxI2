bc6287fc43aed3d39722ac01fd8ad7ac58aec9b7
StefanPatman <stefanpatman91@gmail.com>
2023-01-19 15:31:36 +0200

    Add FileInfo

--- src/itaxotools/taxi3/files.py
+++ src/itaxotools/taxi3/files.py
@@ -4,6 +4,10 @@ from typing import Callable
 from enum import Enum, auto
 from pathlib import Path
 from re import fullmatch
+from dataclasses import dataclass, asdict
+
+from .types import Type
+from .handlers import FileHandler
 
 
 class Identifier:
@@ -17,14 +21,65 @@ class Identifier:
             return bool(fullmatch(r'([^\t]+\t)+[^\t]+', line))
 
 
-class FileFormat(Enum):
-    Fasta = auto(), Identifier.isFasta
-    Tabfile = auto(), Identifier.isTabFile
-    Excel = auto(), lambda _: False
-    Unknown = auto(), lambda _: True
+@dataclass
+class FileInfo(Type):
+    path: Path
+    format: FileFormat
+    size: int
 
-    def __init__(self, _, identifier):
-        self.identifier = identifier
+    @classmethod
+    def from_path(cls, path: Path) -> FileInfo:
+        format = FileFormat.identify(path)
+        return format.info.get(path)
+
+    @classmethod
+    def get(cls, path: Path):
+        return cls(
+            path = path,
+            format = FileFormat.identify(path),
+            size = path.stat().st_size,
+        )
+
+
+@dataclass
+class Tabfile(FileInfo):
+    headers: list[str]
+    header_id: str | None
+    header_seq: str | None
+    header_species: str | None
+    header_genus: str | None
+
+    @classmethod
+    def get(cls, path: Path):
+        headers = FileHandler.Tabfile(path, has_headers=True).headers
+        header_id = 'id' if 'id' in headers else None
+        header_seq = 'seq' if 'seq' in headers else None
+        header_species = 'organism' if 'organism' in headers else None
+        header_genus = 'organism' if 'organism' in headers else None
+
+        return cls(
+            headers = list(headers),
+            header_id = header_id,
+            header_seq = header_seq,
+            header_species = header_species,
+            header_genus = header_genus,
+            **asdict(FileInfo.get(path))
+        )
+
+
+class FileFormat(Enum):
+    Fasta = 'Fasta', Identifier.isFasta, FileInfo
+    Tabfile = 'Tabfile', Identifier.isTabFile, FileInfo.Tabfile
+    Excel = 'Excel', None, None
+    Unknown = 'Unknown', None, None
+
+    def __init__(self, label, identifier, info):
+        self.label = label
+        self.identifier = identifier or (lambda _: False)
+        self.info = info or FileInfo
+
+    def __repr__(self):
+        return f'<{type(self).__name__}.{self._name_}>'
 
     @classmethod
     def identify(cls, path: Path) -> FileFormat:
--- tests/test_files.py
+++ tests/test_files.py
@@ -6,7 +6,7 @@ from typing import Callable, NamedTuple
 import pytest
 from utility import assert_eq_files
 
-from itaxotools.taxi3.files import FileFormat
+from itaxotools.taxi3.files import FileFormat, FileInfo
 
 TEST_DATA_DIR = Path(__file__).parent / Path(__file__).stem
 
@@ -24,11 +24,36 @@ class IdentifyTest(NamedTuple):
         assert result == self.format
 
 
+class InfoTest(NamedTuple):
+    input: str
+    infos: dict[str, object]
+
+    @property
+    def input_path(self) -> Path:
+        return TEST_DATA_DIR / self.input
+
+    def validate(self) -> None:
+        result = FileInfo.from_path(self.input_path)
+        assert result.path == self.input_path
+        for info in self.infos:
+            assert getattr(result, info) == self.infos[info]
+
+
 @pytest.mark.parametrize(
     "test", [
     IdentifyTest(FileFormat.Fasta, 'simple.fasta'),
     IdentifyTest(FileFormat.Tabfile, 'simple.tsv'),
     IdentifyTest(FileFormat.Unknown, 'empty.txt'),
 ])
-def test_write_sequences(test: IdentifyTest) -> None:
+def test_identify_file(test: IdentifyTest) -> None:
+    test.validate()
+
+
+@pytest.mark.parametrize(
+    "test", [
+    InfoTest('simple.fasta', dict(format=FileFormat.Fasta, size=15)),
+    InfoTest('simple.tsv', dict(format=FileFormat.Tabfile, size=21, headers=['id', 'seq'])),
+    InfoTest('empty.txt', dict(format=FileFormat.Unknown, size=0)),
+])
+def test_get_file_info(test: InfoTest) -> None:
     test.validate()
