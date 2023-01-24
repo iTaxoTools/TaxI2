70566497576ab85225c8d116536263719c308f9d
StefanPatman <stefanpatman91@gmail.com>
2023-01-25 00:44:10 +0200

    Add decontaminate2 submodule

--- scripts/decontaminate2.py
+++ scripts/decontaminate2.py
@@ -1,118 +1,27 @@
-from itertools import groupby
 from pathlib import Path
 from sys import argv
-from time import perf_counter
-import os
-from itaxotools.taxi3.align import *
-from itaxotools.taxi3.distances import *
-from itaxotools.taxi3.pairs import *
-from itaxotools.taxi3.sequences import *
+
+from itaxotools.taxi3.tasks.decontaminate2 import Decontaminate2
+from itaxotools.taxi3.sequences import Sequences, SequenceHandler
 
 
-def calc(aligned_pairs, metric=DistanceMetric.Uncorrected()):
-    for x, y in aligned_pairs:
-        yield metric.calculate(x, y)
-
-
-def progress(distances, total):
-
-    for index, distance in enumerate(distances):
-        index+=1
-        print(f"\r Loading... {int(index)}/{total} = {(round(float(index)/float(total)  * 100, 2))} ", end="")
-        yield distance
-
-
-def decontaminateSeq(allpairs, contaminatePath, decontaminatePath, summaryPath, outgroup_weight=1.0):
-
-    decontFile = SequenceFile.Tabfile(decontaminatePath)
-    contFile = SequenceFile.Tabfile(contaminatePath)
-
-    with decontFile.open('w') as decontHandler, contFile.open('w') as contHandler, open(summaryPath, 'w') as summryFile:
-        summryFile.write("seqid_query\tclosest possible inGroup id\tingroup distance\tclosest possible outGroup id\toutgroup distance\tis_contaminant")
-
-        for inputData, ingroup_disctance, outgroup_disctance in allpairs:
-            isContaminate = ingroup_disctance.d > (outgroup_weight * outgroup_disctance.d)
-
-            if isContaminate:
-                decontHandler.write(inputData)
-            else:
-                contHandler.write(inputData)
-
-            summryFile.write(f'\n{ingroup_disctance.x.id}\t{ingroup_disctance.y.id}\t{ingroup_disctance.d}\t{outgroup_disctance.y.id}\t{outgroup_disctance.d}\t{isContaminate}')
-
-            yield ingroup_disctance
-
-
-def get_minimum(distances):
-    for k, g in groupby(distances, lambda d: d.x.id):
-        g = (d for d in g if d.d is not None)
-        d = min(g, key = lambda x: x.d)
-        yield d
-
-
-def multiply(g, n):
-    return (x for x in g for i in range(n))
-
-
-def normalizePairs(pairs):
-    for pair in pairs:
-        yield SequencePair(pair.x.normalize(), pair.y.normalize())
-
-
-def main():
-
-    path_data = Path(argv[1])
-    path_ingroup_reference = Path(argv[2])
-    path_outgroup_reference = Path(argv[3])
-    contaminatePath = Path(argv[4])
-    decontaminatePath = Path(argv[5])
-    summaryPath = Path(argv[6])
-
-    ts = perf_counter()
-
-    file_data = SequenceFile.Tabfile(path_data)
-    file_ingroup_reference = SequenceFile.Tabfile(path_ingroup_reference)
-    file_outgroup_reference = SequenceFile.Tabfile(path_outgroup_reference)
-
-    data = Sequences.fromFile(file_data, idHeader='seqid', seqHeader='sequence')
-    ingroup_reference = Sequences.fromFile(file_ingroup_reference, idHeader='seqid', seqHeader='sequence')
-    outgroup_reference = Sequences.fromFile(file_outgroup_reference, idHeader='seqid', seqHeader='sequence')
-
-    total = len(data)
-
-    in_pairs = SequencePairs.fromProduct(data, ingroup_reference)
-    out_pairs = SequencePairs.fromProduct(data, outgroup_reference)
-
-    #Ingroup
-    normalizeInPair = normalizePairs(in_pairs)
-    aligner = PairwiseAligner.Biopython()
-    aligned_in_pairs = aligner.align_pairs(normalizeInPair)
-
-    in_distances = calc(aligned_in_pairs)
-
-    in_minimums = get_minimum(in_distances)
-
-    #outGroup
-    normalizeOutPair = normalizePairs(out_pairs)
-    aligned_out_pairs = aligner.align_pairs(normalizeOutPair)
-
-    out_distances = calc(aligned_out_pairs)
-
-    out_minimums = get_minimum(out_distances)
-
-    allPairs = zip(data, in_minimums, out_minimums)
-
-    d = decontaminateSeq(allPairs, contaminatePath, decontaminatePath, summaryPath)
-
-    distance = progress(d, total)
-
-    for _ in distance:
-        pass
-
-    tf = perf_counter()
-
-    print(f'Time taken: {tf-ts:.4f}s')
+def main(data_path: Path, outgroup_path: Path, ingroup_path: Path, output_path: Path):
+    task = Decontaminate2()
+    task.work_dir = Path(output_path)
+    task.input = Sequences.fromPath(data_path, SequenceHandler.Tabfile, idHeader='seqid', seqHeader='sequence')
+    task.outgroup = Sequences.fromPath(outgroup_path, SequenceHandler.Tabfile, idHeader='seqid', seqHeader='sequence')
+    task.ingroup = Sequences.fromPath(ingroup_path, SequenceHandler.Tabfile, idHeader='seqid', seqHeader='sequence')
+    task.params.weights.outgroup = 1
+    task.params.weights.ingroup = 1
+    results = task.start()
+    print('')
+    print(f'Output directory: {results.output_directory}')
+    print(f'Time taken: {results.seconds_taken:.4f}s')
 
 
 if __name__ == '__main__':
-    main()
+    data_path = Path(argv[1])
+    outgroup_path = Path(argv[2])
+    ingroup_path = Path(argv[3])
+    output_path = Path(argv[4])
+    main(data_path, outgroup_path, ingroup_path, output_path)
---
+++ src/itaxotools/taxi3/tasks/decontaminate2.py
@@ -0,0 +1,348 @@
+from __future__ import annotations
+
+from itertools import groupby, product, chain
+from pathlib import Path
+from time import perf_counter
+from statistics import mean, median, stdev
+from typing import NamedTuple, TextIO, Callable
+from math import inf
+
+from itaxotools.common.utility import AttrDict
+
+from ..align import PairwiseAligner
+from ..distances import Distances, DistanceHandler, DistanceMetric
+from ..pairs import SequencePair, SequencePairs, SequencePairHandler
+from ..sequences import Sequence, Sequences, SequenceHandler
+from ..partitions import Partition, PartitionHandler
+from ..statistics import StatisticsCalculator, StatisticsHandler
+from ..handlers import FileHandler, ReadHandle, WriteHandle
+from ..plot import HistogramPlotter
+
+
+def multiply(iterator: iter, n: int):
+    return (item for item in iterator for i in range(n))
+
+
+def split(source: iter, *funcs: list[Callable]):
+    source = multiply(source, len(funcs))
+    return [
+        map(func, source)
+        for func in funcs
+    ]
+
+
+def console_report(caption, index, total):
+    if caption == 'Finalizing...':
+        print(f"\rCalculating... {total}/{total} = {100:.2f}%", end="")
+        print('\nFinalizing...')
+    else:
+        print(f"\rCalculating... {index}/{total} = {100*index/total:.2f}%", end="")
+
+
+class Verdict(NamedTuple):
+    sequence: Sequence
+    contaminant: bool
+
+
+class SummaryLine(NamedTuple):
+    query_id: str
+    outgroup_id: str
+    outgroup_distance: float
+    ingroup_id: str
+    ingroup_distance: float
+    contaminant: bool
+
+
+class Results(NamedTuple):
+    output_directory: Path
+    seconds_taken: float
+
+
+class SummaryHandle(FileHandler[SummaryLine]):
+    def _open(
+        self,
+        path: Path,
+        mode: 'r' | 'w' = 'w',
+        formatter: str = '{:f}',
+        *args, **kwargs
+    ):
+        self.formatter = formatter
+        super()._open(path, mode, *args, **kwargs)
+
+    def _iter_read(self, *args, **kwargs) -> ReadHandle[SummaryLine]:
+        raise NotImplementedError()
+
+    def format_line(self, line: SummaryLine):
+        return (
+            line.query_id,
+            line.outgroup_id,
+            self.formatter.format(line.outgroup_distance),
+            line.query_id,
+            self.formatter.format(line.ingroup_distance),
+            'Yes' if line.contaminant else 'No',
+        )
+
+    def _iter_write(self, *args, **kwargs) -> WriteHandle[SummaryLine]:
+        try:
+            with FileHandler.Tabfile(self.path, 'w') as file:
+                while True:
+                    line = yield
+                    file.write(self.format_line(line))
+        except GeneratorExit:
+            return
+
+
+class Decontaminate2:
+
+    def __init__(self):
+
+        self.work_dir: Path = None
+        self.paths = AttrDict()
+
+        self.progress_handler: Callable = console_report
+        self.progress_interval: float = 0.015
+
+        self.input: Sequences = None
+        self.outgroup: Sequences = None
+        self.ingroup: Sequences = None
+
+        self.params = AttrDict()
+
+        self.params.weights = AttrDict()
+        self.params.weights.outgroup: float = 1.0
+        self.params.weights.ingroup: float = 1.0
+
+        self.params.pairs = AttrDict()
+        self.params.pairs.align: bool = True
+        self.params.pairs.write: bool = True
+        self.params.pairs.scores: Scores = None
+
+        self.params.distances = AttrDict()
+        self.params.distances.metric: DistanceMetric = None
+        self.params.distances.write_linear: bool = True
+        self.params.distances.write_matricial: bool = True
+
+        self.params.format = AttrDict()
+        self.params.format.float: str = '{:.4f}'
+        self.params.format.missing: str = 'NA'
+        self.params.format.percentage_multiply: bool = False
+
+    def check_metric(self):
+        self.params.distances.metric = self.params.distances.metric or DistanceMetric.Uncorrected()
+
+    def generate_paths(self):
+        assert self.work_dir
+        self.create_parents(self.work_dir)
+        metric = str(self.params.distances.metric)
+        extension = '.tsv'
+
+        self.paths.summary = self.work_dir / 'summary.tsv'
+        self.paths.decontaminated = self.work_dir / f'decontaminated{extension}'
+        self.paths.contaminants = self.work_dir / f'contaminants{extension}'
+        self.paths.aligned_pairs = self.work_dir / 'aligned_pairs.txt'
+        self.paths.outgroup_linear = self.work_dir / 'distances' / f'outgroup.{metric}.linear.tsv'
+        self.paths.outgroup_matrix = self.work_dir / 'distances' / f'outgroup.{metric}.matricial.tsv'
+        self.paths.ingroup_linear = self.work_dir / 'distances' / f'ingroup.{metric}.linear.tsv'
+        self.paths.ingroup_matrix = self.work_dir / 'distances' / f'ingroup.{metric}.matricial.tsv'
+
+    def create_parents(self, path: Path):
+        if path.suffix:
+            path = path.parent
+        path.mkdir(parents=True, exist_ok=True)
+
+    def align_pairs(self, pairs: iter[SequencePair]) -> iter[SequencePair]:
+        if not self.params.pairs.align:
+            yield from pairs
+            return
+
+        aligner = PairwiseAligner.Biopython(self.params.pairs.scores)
+        yield from aligner.align_pairs(pairs)
+
+    def write_pairs(self, pairs: iter[SequencePair]) -> iter[SequencePair]:
+        if not self.params.pairs.write:
+            yield from pairs
+            return
+
+        self.create_parents(self.paths.aligned_pairs)
+        with SequencePairHandler.Formatted(self.paths.aligned_pairs, 'w') as file:
+            for pair in pairs:
+                file.write(pair)
+                yield pair
+
+    def calculate_distances(self, pairs: iter[SequencePair]) -> iter[Distance]:
+        metric = self.params.distances.metric
+        for x, y in pairs:
+            yield metric.calculate(x, y)
+
+    def adjust_distances(self, distances: iter[Distance]) -> iter[Distance]:
+        if not self.params.format.percentage_multiply:
+            yield from distances
+            return
+
+        for distance in distances:
+            distance = distance._replace(d = distance.d * 100)
+            yield distance
+
+    def write_distances_linear(self, distances: iter[Distance], path: Path) -> iter[Distance]:
+        if not self.params.distances.write_linear:
+            yield from distances
+            return
+
+        self.create_parents(path)
+        with DistanceHandler.Linear.WithExtras(
+            path, 'w',
+            missing = self.params.format.missing,
+            formatter = self.params.format.float,
+        ) as file:
+            for distance in distances:
+                file.write(distance)
+                yield distance
+
+    def write_outgroup_distances_linear(self, distances: iter[Distance]) -> iter[Distance]:
+        return self.write_distances_linear(distances, self.paths.outgroup_linear)
+
+    def write_ingroup_distances_linear(self, distances: iter[Distance]) -> iter[Distance]:
+        return self.write_distances_linear(distances, self.paths.ingroup_linear)
+
+    def write_distances_matrix(self, distances: iter[Distance], path: Path) -> iter[Distance]:
+        if not self.params.distances.write_matricial:
+            yield from distances
+            return
+
+        self.create_parents(path)
+        with DistanceHandler.Matrix(
+            path, 'w',
+            missing = self.params.format.missing,
+            formatter = self.params.format.float,
+        ) as file:
+            for distance in distances:
+                file.write(distance)
+                yield distance
+
+    def write_outgroup_distances_matrix(self, distances: iter[Distance]) -> iter[Distance]:
+        return self.write_distances_matrix(distances, self.paths.outgroup_matrix)
+
+    def write_ingroup_distances_matrix(self, distances: iter[Distance]) -> iter[Distance]:
+        return self.write_distances_matrix(distances, self.paths.ingroup_matrix)
+
+    def group_distances_left(self, distances: iter[Distance]) -> iter[iter[Distance]]:
+        for _, group in groupby(distances, lambda distance: distance.x.id):
+            yield group
+
+    def get_minimum_distances(self, groups: iter[iter[Distance]]) -> iter[Distance]:
+        for distances in groups:
+            distances = (distance for distance in distances if distance.d is not None)
+            minimum_distance = min(distances, key = lambda distance: distance.d)
+            yield minimum_distance
+
+    def _find_contaminants(
+        self,
+        sequences: iter[Sequence],
+        out_minimums: iter[Distance],
+        in_minimums: iter[Distance],
+    ) -> iter[tuple[Verdict, SummaryLine]]:
+
+        outgroup_weight = self.params.weights.outgroup
+        ingroup_weight = self.params.weights.ingroup
+        all = zip(sequences, out_minimums, in_minimums)
+        for sequence, outgroup_minimum, ingroup_minimum in all:
+            outgroup_distance = outgroup_minimum.d * outgroup_weight
+            ingroup_distance = ingroup_minimum.d * ingroup_weight
+            is_contaminant = bool(outgroup_distance <= ingroup_distance)
+            verdict = Verdict(sequence, is_contaminant)
+            line = SummaryLine(
+                query_id = sequence.id,
+                outgroup_id = outgroup_minimum.y.id,
+                outgroup_distance = outgroup_distance,
+                ingroup_id = ingroup_minimum.y.id,
+                ingroup_distance = ingroup_distance,
+                contaminant = is_contaminant,
+            )
+            yield (verdict, line)
+
+    def find_contaminants(
+        self,
+        sequences: iter[Sequence],
+        out_minimums: iter[Distance],
+        in_minimums: iter[Distance],
+    ) -> tuple[iter[Verdict], iter[SummaryLine]]:
+
+        data = self._find_contaminants(sequences, out_minimums, in_minimums)
+        return split(data, lambda x: x[0], lambda x: x[1])
+
+    def write_file_decontaminated(self, verdicts: iter[Verdict]) -> iter[Verdict]:
+        with SequenceHandler.Tabfile(self.paths.decontaminated, 'w') as file:
+            for verdict in verdicts:
+                if not verdict.contaminant:
+                    file.write(verdict.sequence)
+                yield verdict
+
+    def write_file_contaminants(self, verdicts: iter[Verdict]) -> iter[Verdict]:
+        with SequenceHandler.Tabfile(self.paths.contaminants, 'w') as file:
+            for verdict in verdicts:
+                if verdict.contaminant:
+                    file.write(verdict.sequence)
+                yield verdict
+
+    def write_summary(self, lines: iter[SummaryLine]) -> iter[SummaryLine]:
+        with SummaryHandle(
+            self.paths.summary, 'w',
+            formatter = self.params.format.float,
+        ) as file:
+            for line in lines:
+                if line is not None:
+                    file.write(line)
+                yield line
+
+    def report_progress(self, verdicts: iter[Verdict]):
+        total = len(self.input)
+        last_time = perf_counter()
+        for index, verdict in enumerate(verdicts, 1):
+            new_time = perf_counter()
+            if new_time - last_time >= self.progress_interval:
+                self.progress_handler('verdict.x.id', index, total)
+                last_time = new_time
+            yield verdict
+        self.progress_handler('Finalizing...', total, total)
+
+    def start(self) -> None:
+        ts = perf_counter()
+
+        self.check_metric()
+        self.generate_paths()
+
+        data = self.input.normalize()
+
+        outgroup = self.outgroup.normalize()
+        out_pairs = SequencePairs.fromProduct(data, outgroup)
+        out_pairs = self.align_pairs(out_pairs)
+        out_pairs = self.write_pairs(out_pairs)
+        out_distances = self.calculate_distances(out_pairs)
+        out_distances = self.adjust_distances(out_distances)
+        out_distances = self.write_outgroup_distances_linear(out_distances)
+        out_distances = self.write_outgroup_distances_matrix(out_distances)
+        out_groups = self.group_distances_left(out_distances)
+        out_minimums = self.get_minimum_distances(out_groups)
+
+        ingroup = self.ingroup.normalize()
+        in_pairs = SequencePairs.fromProduct(data, ingroup)
+        in_pairs = self.align_pairs(in_pairs)
+        in_pairs = self.write_pairs(in_pairs)
+        in_distances = self.calculate_distances(in_pairs)
+        in_distances = self.write_ingroup_distances_linear(in_distances)
+        in_distances = self.write_ingroup_distances_matrix(in_distances)
+        in_groups = self.group_distances_left(in_distances)
+        in_minimums = self.get_minimum_distances(in_groups)
+
+        verdicts, lines = self.find_contaminants(data, out_minimums, in_minimums)
+        verdicts = self.write_file_decontaminated(verdicts)
+        verdicts = self.write_file_contaminants(verdicts)
+        verdicts = self.report_progress(verdicts)
+        lines = self.write_summary(lines)
+
+        for _ in zip(verdicts, lines):
+            pass
+
+        tf = perf_counter()
+
+        return Results(self.work_dir, tf - ts)
